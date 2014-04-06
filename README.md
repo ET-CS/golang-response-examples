fe9c179498c05935279ca634a9151283833ed25b
ET-CS <et.programming@gmail.com>
2014-04-06 19:18:43 +0300

    Minimal implementation

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ golang-response-examples
 ========================
 
 Golang examples with several client\server responses.
+
+## Minimal
+
+Simple minimal Golang web-server
+
+> git run minimal.go
+
+Visit *http://127.0.0.1:8080/My-Name*
---
+++ minimal.go
@@ -0,0 +1,19 @@
+// Miminal golang web-server implementation
+// Visit: http://127.0.0.1:8080/Your-name
+package minimal 
+
+import (
+    "net/http"
+    "fmt"
+)
+
+// Default Request Handler
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", defaultHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
